579da8210f1c100ca30ed7c8e50e787376daa316
Kent Tokunaga <kenttokunaga@gmail.com>
2018-09-11 14:35:43 -0400

    Get note by ID

--- scratch/queries.js
+++ scratch/queries.js
@@ -2,19 +2,31 @@
 
 const knex = require('../knex');
 
-let searchTerm = 'gaga';
+// let searchTerm = 'gaga';
+// knex
+//   .select('notes.id', 'title', 'content')
+//   .from('notes')
+//   .modify(queryBuilder => {
+//     if (searchTerm) {
+//       queryBuilder.where('title', 'like', `%${searchTerm}%`);
+//     }
+//   })
+//   .orderBy('notes.id')
+//   .then(results => {
+//     console.log(JSON.stringify(results, null, 2));
+//   })
+//   .catch(err => {
+//     console.error(err);
+//   });
+
+
+// get notes by ID
+process.stdout.write('\x1Bc');
+
+let id = '1005';
 knex
-  .select('notes.id', 'title', 'content')
+  .select()
   .from('notes')
-  .modify(queryBuilder => {
-    if (searchTerm) {
-      queryBuilder.where('title', 'like', `%${searchTerm}%`);
-    }
-  })
-  .orderBy('notes.id')
-  .then(results => {
-    console.log(JSON.stringify(results, null, 2));
-  })
-  .catch(err => {
-    console.error(err);
-  });
+  .where({id: id})
+  .then(results => console.log(JSON.stringify(results[0], null, 2)))
+  .catch(err => console.error(err));
